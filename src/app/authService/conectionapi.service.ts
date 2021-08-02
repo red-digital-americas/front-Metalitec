e8b9bb45726ea9fdb41fecf5b183c0eb33b91105
Julian050693 <72213519+Julian050693@users.noreply.github.com>
2021-08-02 16:49:24 -0500

    Get, Put, Delete, Post

--- src/app/authService/conectionapi.service.ts
+++ src/app/authService/conectionapi.service.ts
@@ -13,6 +13,22 @@ export class ConectionapiService {
 
   constructor(private http: HttpClient) { }
 
+  service_general_post_with_url(url:string, parametros:string): Observable<any> {
+    return this.http.post(this.url_api + url, parametros, { headers: this.headers });
+  }
+
+  public service_general_put(url:string, parametros:string): Observable<any> {
+    return this.http.put(this.url_api + url, parametros, { headers: this.headers });
+  }
+
+  public service_general_delete(url:string):Observable<any> {
+    return this.http.delete(this.url_api + url, { headers: this.headers });
+  }
+
+  public service_general_delete_with_url(url:string): Observable<any> {
+    return this.http.delete(this.url_api + url, { headers: this.headers });
+  }
+
   public service_general_get(url:string): Observable<any> {
     return this.http.get(this.url_api + url, { headers: this.headers });
   }
